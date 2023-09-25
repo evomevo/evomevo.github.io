aa1347edaaee9e4db0883fa25a87799169987c8f
evomevo <131975584+evomevo@users.noreply.github.com>
2023-09-25 20:15:04 +0800

    script optimizations
    los geht's

---
+++ scripts.js
@@ -0,0 +1,108 @@
+//dropdown gallery
+const dropbtnA = document.querySelectorAll("#dropbtnA");
+const imglayoutA = document.querySelector("#imglayoutA");
+
+const dropbtnB = document.querySelectorAll("#dropbtnB");
+const imglayoutB = document.querySelector("#imglayoutB");
+
+dropbtnA.forEach(dropbtnA => {
+  dropbtnA.addEventListener('click', function() {
+    if (dropbtnA.textContent === 'Click to reveal') {
+      dropbtnA.innerHTML = '<p>Click to hide</p>';
+    } else {
+      dropbtnA.innerHTML = '<p>Click to reveal</p>';
+    };
+    setTimeout(() => {
+      imglayoutA.classList.toggle('active');
+    }, 200);
+  });
+});
+
+dropbtnB.forEach(dropbtnB => {
+  dropbtnB.addEventListener('click', function() {
+    if (dropbtnB.textContent === 'Click to reveal') {
+      dropbtnB.innerHTML = '<p>Click to hide</p>';
+    } else {
+      dropbtnB.innerHTML = '<p>Click to reveal</p>';
+    };
+    setTimeout(() => {
+      imglayoutB.classList.toggle('active');
+    }, 200);
+  });
+});
+
+
+
+//gallery image
+const galimg = document.querySelectorAll('.galimg');
+const imgmodal = document.querySelector('.imgmodal');
+
+galimg.forEach(galimg => {
+  galimg.addEventListener('click', () => {
+    const activeGalimg = document.querySelector('.galimg.active');
+    if (activeGalimg) {
+      setTimeout(() => {
+        activeGalimg.remove();
+      }, 100);
+    }
+
+    const clone = galimg.cloneNode(true);
+    clone.classList.add('active');
+    clone.id = 'tilt';
+
+    imgmodal.appendChild(clone);
+    imgmodal.style.display = 'flex';
+
+    setTimeout(() => {
+      imgmodal.style.opacity = 1;
+    }, 100);
+  });
+});
+
+document.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('galimg')) {
+    const activeGalimg = document.querySelector('.galimg.active');
+    if (activeGalimg) {
+      imgmodal.style.opacity = 0;
+      setTimeout(() => {
+        activeGalimg.remove();
+        imgmodal.style.display = 'none';
+      }, 100);
+    }
+  }
+});
+
+
+
+//hamburger menu
+const hamburger = document.querySelector('.hamburger');
+const navmenu = document.querySelector('.navmenu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navmenu.classList.toggle('active');
+});
+
+document.body.addEventListener('click', (event) => {
+  if (event.target !== hamburger && event.target !== navmenu && event.target.closest('.hamburger') !== hamburger) {
+    hamburger.classList.remove('active');
+    navmenu.classList.remove('active');
+  }
+});
+
+//header scroll
+document.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  const logo = document.querySelector('.logo');
+  const bars = document.querySelectorAll('.bar');
+
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+    logo.classList.add('scrolled');
+    bars.forEach(bar => bar.classList.add('scrolled'));
+  } else {
+    header.classList.remove('scrolled');
+    logo.classList.remove('scrolled');
+    bars.forEach(bar => bar.classList.remove('scrolled'));
+  }
+});
