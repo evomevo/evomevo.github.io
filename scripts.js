a85c77518417f489581aa4efbdf70c614eb9ec94
evomevo <131975584+evomevo@users.noreply.github.com>
2023-09-26 21:07:37 +0800

    win10 hover effect for containers
    WOWOWOWOWOW

--- scripts.js
+++ scripts.js
@@ -63,6 +63,23 @@ document.body.addEventListener('click', (event) => {
 
 
 
+//container hover effect
+const parentCont = document.querySelector(".container");
+
+parentCont.addEventListener('mousemove', (event) => {
+  const containers = document.querySelectorAll(".radgradient");
+  containers.forEach(container => {
+    const rect = container.getBoundingClientRect();
+    x = event.clientX - rect.left;
+    y = event.clientY - rect.top;
+
+    container.style.setProperty("--mouse-x", `${x}px`);
+    container.style.setProperty("--mouse-y", `${y}px`);
+  });
+});
+
+
+
 //dropdown gallery
 const dropbtnA = document.querySelectorAll("#dropbtnA");
 const imglayoutA = document.querySelector("#imglayoutA");
